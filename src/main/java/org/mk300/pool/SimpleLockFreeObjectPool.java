a33116a376a7fd8fb9ef8c486a7cbb48236384f8
mkobayas <mkobayas@redhat.com>
2014-07-03 19:00:05 +0900

    Add License term and author

--- src/main/java/org/mk300/pool/SimpleLockFreeObjectPool.java
+++ src/main/java/org/mk300/pool/SimpleLockFreeObjectPool.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Masazumi Kobayashi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.mk300.pool;
 
 import java.util.concurrent.Semaphore;
@@ -6,7 +22,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
-
+/**
+ * 
+ * @author mkobayas@redhat.com
+ *
+ * @param <T>
+ */
 public class SimpleLockFreeObjectPool<T> {
 
 	static final int ST_NULL= 0;
